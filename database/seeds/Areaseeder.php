566ea7dce86d339040c16d0874398b64d3cc2a88
tomii1031 <57342235+tomii1031@users.noreply.github.com>
2020-07-09 15:08:00 +0900

    first commit

---
+++ database/seeds/Areaseeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Areaseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
